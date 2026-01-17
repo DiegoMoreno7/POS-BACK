a0cd6132d9033da613bc8155d59541d4dc25d14d
DiegoMoreno7 <diegomorenoduran1@gmail.com>
2026-01-16 19:33:08 -0700

    inicio

---
+++ src/main/java/com/example/posback/PosBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.posback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/posback/PosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.posback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
